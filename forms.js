a8b8a124d8a93a73c89755f038f955fbecdc9230
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-10-17 21:42:53 -0500

    form processing

---
+++ forms.js
@@ -0,0 +1,10 @@
+function myProcessForm(event) {
+    document.write("Form Processed");
+    }
+
+ function myProcessForm(event) {
+        const form = event.target;
+        const input = form[0];
+            const value = input.value;
+        output( form );
+     }
---
+++ utility-functions.js
@@ -0,0 +1,4 @@
+function display(message) {
+    document.write(message);
+}
+
