79004e27c47950248a13d17502e71729b969a8c1
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-05 10:56:32 -0600

    JS notes

--- javascript.js
+++ javascript.js
@@ -23,3 +23,22 @@ function asynchronous () {
     setTimeout(greeting, 5000);
     display("Waiting for the greeting...");
 }
+
+function submitForm(event) {
+    const inputs = event.target;
+    const emailInput = inputs[0];
+    const email = emailInput.value;
+    display("Submitting for " + email + " ...");
+    setTimeout(serverResponse, 5000);
+}
+
+function serverResponse() {
+   display("Form was successfully processed.");
+}
+
+function displayObject() {
+    const myCar = { color: "red", year: 2024 };
+    const result = JSON.stringify(myCar);
+    display(result);
+}
+
