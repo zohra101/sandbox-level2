369a2105fa840c89f0430be6f38f1754ede2b4c2
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-04 20:26:04 -0600

    JS

--- javascript.js
+++ javascript.js
@@ -1,3 +1,25 @@
 function display(message) {
     document.write(message);
 }
+
+function greeting () {
+    display("Hello World!");
+}
+
+function myFunction(callbackFunction){
+    callbackFunction();
+}
+function activator(){
+    myFunction(greeting);
+}
+activator();
+
+function dekayedGreeting () {
+    setTimeout(greeting, 300);
+
+}
+
+function asynchronous () {
+    setTimeout(greeting, 5000);
+    display("Waiting for the greeting...");
+}
