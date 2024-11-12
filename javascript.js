5f78595fb91038735647ca5f99f8f36c0ba26ae3
zohra101 <alex.marjanovic.ba.tw@gmail.com>
2024-11-12 16:37:52 -0600

    Updated promise and resolve notes

--- javascript.js
+++ javascript.js
@@ -1,7 +1,10 @@
+//Display message
 function display(message) {
     document.write(message);
 }
 
+
+//Class v. object
 // function classVObject() {
 //         class MyCar {
 //             color = "red";
@@ -14,19 +17,34 @@ function display(message) {
 // display(MyCar);
 // display(myCar);
 
+//Create object fron class
+// const myCar = new MyCar();
+// const myCar2 = new MyCar();
+// const myCar3 = new MyCar();
 
-function createObjectFromClass() {
-    const myCar = new MyCar();
-    display(myCar);
+// myCar2.color = "white";
+// myCar3.year = 2025;
+        
+function newObject() {
+const myCar = new MyCar();
+const myCar2 = new MyCar();
+const myCar3 = new MyCar();
+display(JSON.strigify(myCar));
+display(JSON.strigify(myCar2));
+display(JSON.strigify(myCar3));
 }
 
+
+//Callback function
+function myFunction(callbackFunction) {
+    callbackFunction();
+}
+
+//Activator functions
 function greeting () {
     display("Hello World!");
 }
 
-function myFunction(callbackFunction) {
-    callbackFunction();
-}
 function activator() {
     myFunction(greeting);
 }
@@ -40,33 +58,72 @@ function activatorTest() {
     display(myParameter);
     }
 
-function dekayedGreeting () {
-    setTimeout(greeting, 300);
-
-}
-
+//Aysnchronous function
 function asynchronous () {
     setTimeout(greeting, 5000);
     display("Waiting for the greeting...");
 }
 
+//Promise
+const results = new Promise(myFunction);
+const promise = new Promise(myFunction);
+
+function makePromise() {
+    const promise = new Promise(myFunction);
+    display("Make a promise.")
+}
+
+//setTimeout function
+function delayedGreeting () {
+    setTimeout(greeting, 300);
+}
+
+//Resolve function
+function resolveFunction(resolve) {
+    setTimeout(serverResponse, 5000);
+    display("Inside my myFunction.");
+}
+
+function madePromise() {
+    const promise = new Promise(resolveFunction);
+    display("Made a promise.")
+}
+
+function serverResponse() {
+   display("Your information has been processed successfully.");
+   resolve();
+}
+
+//Nested function 
+function outerFunction () {
+    const message = "Hello world!";
+    innerFunction();
+
+    function innerFunction () {
+        display(message);
+    }
+}
+
+//Nested resolve function
+
+
+//Form processing message
 function submitForm(event) {
     const inputs = event.target;
     const emailInput = inputs[0];
     const email = emailInput.value;
-    display("Submitting for " + email + " ...");
+    display("Submitting for " + email + "...");
     setTimeout(serverResponse, 5000);
 }
 
-function serverResponse() {
-   display("Form was successfully processed.");
-}
-
+//
 function displayObject() {
     const myCar = { color: "red", year: 2024 };
     const result = JSON.stringify(myCar);
     display(result);
 }
+
+
 
 // class MyCar {
 //     color = "red";
